54968b12a2851948d81e5f69baf303b6e6d70cc4
bmolina-nyc <bruce.molina.81@gmail.com>
2019-04-10 16:04:47 -0400

    updated init account and util

--- app/account.py
+++ app/account.py
@@ -4,7 +4,7 @@ from app.util import hash_password
 
 class Account(ORM):
     tablename = "accounts"
-    fields = ["username", "password_hash", ]
+    fields = ["username", "password_hash"]
 
     def __init__(self, *args, **kwargs):
             self.pk = kwargs.get('pk')
@@ -12,7 +12,11 @@ class Account(ORM):
             self.password_hash = kwargs.get('password_hash')
   
     def set_password(self, password):
+        print(password)
+        input()
         hashed_pw = hash_password(password)
+        print(hashed_pw)
+        input()
         self.password_hash = hashed_pw 
         return hashed_pw
 
