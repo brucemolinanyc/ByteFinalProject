2293419c2bcaf842b67310c31015d9958a274212
bmolina-nyc <bruce.molina.81@gmail.com>
2019-04-11 13:33:28 -0400

    updated login

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,5 +1,6 @@
 from flask import jsonify, abort, request
 from flask_app import app
+from app.account import Account
 
 UNAUTHORIZED = {"error": "unauthorized", "status_code": 401}
 NOT_FOUND = {"error": "not found", "status_code": 404}
@@ -9,3 +10,12 @@ BAD_REQUEST = {"error": "bad request", "status_code": 400}
 @app.route('/', methods=['GET'])
 def home():
     return jsonify({"test": "working"})
+
+@app.route('/login', methods=['POST'])
+def login():
+    if not request.json or 'username' not in request.json or 'password' not in request.json:
+        return jsonify(BAD_REQUEST), 401
+    account = Account.login(request.json['username'], request.json['password'])
+    if not account:
+        return jsonify(UNAUTHORIZED), 401
+    return jsonify({"login": "working"})
