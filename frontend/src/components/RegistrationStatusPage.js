74c4471290a28e56c76b69897cf3597c888e14bc
bmolina-nyc <bruce.molina.81@gmail.com>
2019-04-15 15:51:01 -0400

    updating registration page

--- frontend/src/components/RegistrationStatusPage.js
+++ frontend/src/components/RegistrationStatusPage.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React from 'react'
+import { Form, Container } from 'semantic-ui-react'
 
-class RegistrationStatusPage extends React.Component{
+const options = [
+  { key: 'm', text: 'Male', value: 'male' },
+  { key: 'f', text: 'Female', value: 'female' }
+]
 
-    render(){
-        return(
-            <div>
-                RegistrationStatusPage
-            </div>
-        )
-    }
+const years = [
+    {key: 'years', text:[...Array(100).keys()]}
+]
 
+const RegistrationStatusPage = () => (
+    <Container>
+        <Form>
+            <Form.Group >
+                <Form.Input fluid label='First name' placeholder='First name' width={4}  />
+                <Form.Input fluid label='Last name' placeholder='Last name' width={4}/>
+                <Form.Select fluid label="Gender" options={options} placeholder='Gender'  width={2}/>
+            </Form.Group>
+            <Form.Group widths=''>
+                <Form.Input fluid label='Street Address' placeholder='Street Address' width={8}  />
+                <Form.Input fluid label='Apt/Unit#' placeholder='Apt/Unit#' width={2}/>
+            </Form.Group>
+            <Form.Group widths=''>
+                <Form.Input fluid label='City' placeholder='City' width={6}  />
+                <Form.Input fluid label='State' placeholder='State' width={2}/>
+                <Form.Input fluid label='Zip' placeholder='Zip' width={2}/>
+            </Form.Group>
+            <Form.Group >
+                <Form.Input fluid label='Month' placeholder='Month' width={4}  />
+                <Form.Input fluid label='Date' placeholder='Date' width={4}/>
+                <Form.Select fluid label="Year" options={years} placeholder='Year'  width={2}/>
+            </Form.Group>
+        </Form>   
+    </Container>
 
+ 
+)
 
-}
-
-export default RegistrationStatusPage;
+export default RegistrationStatusPage
 
 
 // To do:
