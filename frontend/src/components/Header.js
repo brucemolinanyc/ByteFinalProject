2cb0ba7836eb68b292d5eb8b46ede3ad3bb885ce
bmolina-nyc <bruce.molina.81@gmail.com>
2019-04-18 10:48:56 -0400

    updated reg page

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -16,12 +16,3 @@ const Header = () => (
 );
 export default Header;
 
-  // <header>
-  //   <h1>TBD Site name here</h1>
-  //   <NavLink to="/" activeClassName="is-active" exact={true}>Login Placeholder </NavLink>
-  //   <NavLink to="/home" activeClassName="is-active" exact={true}>Home</NavLink>
-  //   <NavLink to="/profile" activeClassName="is-active">Profile</NavLink>
-  //   <NavLink to="/vote" activeClassName="is-active">Check Voter Registration</NavLink>
-
-  // </header>
-
